4be46bd8b446de55ab3baadb84ed74c4b54e8e3d
tenley22 <tenleyflint@gmail.com>
2021-10-01 09:28:28 -0400

    updated w/ template

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
+# go to terminal (@ bottom of screen) and write "pip install pygame" after PS C: line
+# USE OBJECT ORIENTED
 import pygame
+import math
+import random
+
+# color constants
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+ORANGE = (255, 50, 0)
+YELLOW = (255, 234, 0)
+GREEN = (10, 100, 10)
+BLUE = (10, 10, 150)
+PURPLE = (70, 0, 70)
+
+TOPE = (70, 50, 60)
+PINK = (250, 70, 90)
+NEW2 = (120, 24, 9)
+
+# math constants
+
+# game constants
+DISPLAY_WIDTH = 700
+DISPLAY_HEIGHT = 500
+FPS = 60
+
+############################################################
+############################################################
+
+pygame.init()
+
+screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+clock = pygame.time.Clock()
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+
+    screen.fill(NEW2)
+
+    clock.tick(FPS)
+
+pygame.quit()
