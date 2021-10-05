875c8b042eb7961c4df91e2fca350ceaf5739052
tenley22 <tenleyflint@gmail.com>
2021-10-05 09:38:35 -0400

    starting classes

--- main.py
+++ main.py
@@ -7,17 +7,12 @@ import random
 # color constants
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-ORANGE = (255, 50, 0)
-YELLOW = (255, 234, 0)
-GREEN = (10, 100, 10)
-BLUE = (10, 10, 150)
-PURPLE = (70, 0, 70)
+RED = (77, 0, 0)
+ORANGE = (166, 45, 0)
+YELLOW = (255, 153, 0)
+BG = (26, 0, 0)
+GREY = (4, 2, 2)
 
-TAUPE = (70, 50, 60)
-PINK = (250, 70, 90)
-
-NEW2 = (85, 15, 10)
 
 # math constants
 
@@ -28,6 +23,35 @@ FPS = 60
 
 ############################################################
 ############################################################
+
+class House:
+
+    def __init__(self, x_val, y_val, color, color2, color3):
+        self.x_val = x_val
+        self.y_val = y_val
+        self.color = color
+        self.color2 = color2
+        self.color3 = color3
+
+    def draw_house(self):
+        pygame.draw.polygon(screen, self.color, ((self.x_val, self.y_val), (self.x_val, self.y_val - 300), (self.x_val+150, self.y_val - 350), (self.x_val+300, self.y_val - 300), (self.x_val+300, self.y_val)))
+        pygame.draw.rect(screen, self.color3, ((self.x_val+20, self.y_val-270),(self.x_val+60, self.y_val-440)))
+        pygame.draw.rect(screen, self.color3, ((self.x_val+220, self.y_val-270), (self.x_val+60, self.y_val-440)))
+        pygame.draw.rect(screen, self.color3, ((self.x_val + 20, self.y_val - 170), (self.x_val + 60, self.y_val - 340)))
+
+house = House(0, DISPLAY_HEIGHT, BLACK, GREY, ORANGE)
+
+class Ground:
+    def __init__(self, x_val, y_val, color):
+        self.x_val = x_val
+        self.y_val = y_val
+        self.color = color
+
+    def draw_ground(self):
+        pygame.draw.rect(screen, self.color, ((self.x_val, self.y_val),(self.x_val+DISPLAY_WIDTH, self.y_val + 50)))
+
+ground = Ground(0, 450, BLACK)
+
 
 pygame.init()
 
@@ -42,17 +66,18 @@ while running:
             running = False
 
     ## functions ##
-    #house
-    def house(place, fill, )
 
 
     ## drawing ##
+    house.draw_house()
+    ground.draw_ground()
+
 
 
 
     pygame.display.flip()
 
-    screen.fill(NEW2)
+    screen.fill(BG)
 
     clock.tick(FPS)
 
